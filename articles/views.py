42f575cab5c2daf363f51b5f876c16f1c8800660
sasha000anl <agaluk22@gmail.com>
2025-10-22 18:13:19 +0300

    fix and logo

--- articles/views.py
+++ articles/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Article, Comment
 from .forms import ArticleForm, CommentForm, RatingForm
 from django.db.models import Q
+from .models import Rating
 
 
 # -----------------------------------
